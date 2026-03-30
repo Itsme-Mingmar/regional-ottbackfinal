941f865785a96c41b6e9e7939a0ddddd083195fe
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-03-30 19:25:45 +0545

    feat(videoroute): arrange different types of route

--- src/routes/videoRoutes.js
+++ src/routes/videoRoutes.js
@@ -1,42 +1,75 @@
 import { Router } from "express";
-import { getAllVideos, getAllMovies, getVideoById, uploadVideo, updateVideo, deleteVideo, getProvinceVideos, getAllProvinces, getNepaliMovies, searchMovies } from "../controllers/video.controller.js";
+import {
+  getAllVideos,
+  getAllMovies,
+  getVideoById,
+  uploadVideo,
+  updateVideo,
+  deleteVideo,
+  getProvinceVideos,
+  getAllProvinces,
+  getNepaliMovies,
+  searchMovies
+} from "../controllers/video.controller.js";
+
 import { uploadVideoAndThumbnail } from "../middleware/multer.middleware.js";
 import verifyJWT from "../middleware/verifyJwt.middleware.js";
 import roleVerify from "../middleware/role.middleware.js";
 
 const videoRouter = Router();
 
-videoRouter.get("/", getAllVideos); // list all videos first to avoid conflict with id route
+// Upload video
+videoRouter.post(
+  "/upload",
+  verifyJWT,
+  roleVerify,
+  uploadVideoAndThumbnail.fields([
+    { name: "video", maxCount: 1 },
+    { name: "thumbnail", maxCount: 1 }
+  ]),
+  uploadVideo
+);
+
+// Get all videos
+videoRouter.get("/", getAllVideos);
+
+// Movies
 videoRouter.get("/movies", getAllMovies);
+
+// Provinces list
 videoRouter.get("/provinces", getAllProvinces);
+
+// Nepali movies
 videoRouter.get("/nepali-movies", getNepaliMovies);
+
+// Search
 videoRouter.get("/search", searchMovies);
-videoRouter.get("/province/:provinceId/:category", getProvinceVideos);
+
+// Province-based videos
+videoRouter.get("/province/:slug/:category", getProvinceVideos);
+
+
+// Get single video
 videoRouter.get("/:videoId", getVideoById);
-videoRouter.post("/upload",
-    verifyJWT,
-    roleVerify,
-    uploadVideoAndThumbnail.fields([
-        { name: 'video', maxCount: 1 },
-        { name: 'thumbnail', maxCount: 1 }
-    ]),
-    uploadVideo
+
+// Update video
+videoRouter.put(
+  "/:videoId",
+  verifyJWT,
+  roleVerify,
+  uploadVideoAndThumbnail.fields([
+    { name: "video", maxCount: 1 },
+    { name: "thumbnail", maxCount: 1 }
+  ]),
+  updateVideo
 );
 
-videoRouter.put("/:videoId",
-    verifyJWT,
-    roleVerify,
-    uploadVideoAndThumbnail.fields([
-        { name: 'video', maxCount: 1 },
-        { name: 'thumbnail', maxCount: 1 }
-    ]),
-    updateVideo
-);
-
-videoRouter.delete("/:videoId",
-    verifyJWT,
-    roleVerify,
-    deleteVideo
+// Delete video
+videoRouter.delete(
+  "/:videoId",
+  verifyJWT,
+  roleVerify,
+  deleteVideo
 );
 
 export { videoRouter };
