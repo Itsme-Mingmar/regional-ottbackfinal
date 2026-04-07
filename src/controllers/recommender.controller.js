de4b735fa4022b163ea2fdfe00358f67838bc6e0
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-04-07 22:18:07 +0545

    feat(recommender.controller): created controller for recommender

---
+++ src/controllers/recommender.controller.js
@@ -0,0 +1,62 @@
+import WatchHistory from "../models/watchHistory.js";
+import Video from "../models/video.js";
+import axios from "axios";
+
+export const getRecommendations = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    // 1. Get watch history 
+    const history = await WatchHistory.find({ user: userId })
+      .sort({ watchedAt: -1 })
+      .limit(10)
+      .populate({
+        path: "video",
+        select: "title category"
+      });
+
+    const userLikes = history
+      .filter(h =>
+        h.video &&                      
+        h.video.title &&               
+        h.video.category === "movie"   
+      )
+      .map(h => h.video.title);
+
+    // 3. Cold start 
+    if (userLikes.length === 0) {
+      const trending = await Video.find({ category: "movie" })
+        .sort({ views: -1 })
+        .limit(4)
+        .select("title thumbnailUrl");
+
+      return res.json(trending);
+    }
+
+    
+    const response = await axios.post("http://localhost:8000/recommend", {
+      user_likes: userLikes
+    });
+
+    const recommendedTitles = response.data.recommendations;
+
+    // 5. Fetch full movie data
+    const movies = await Video.find({
+      title: { $in: recommendedTitles },
+      category: "movie" // 🔥 safety
+    }).select("title thumbnailUrl");
+
+    // 6. Maintain order (IMPORTANT)
+    const moviesMap = new Map(movies.map(m => [m.title, m]));
+
+    const orderedMovies = recommendedTitles
+      .map(title => moviesMap.get(title))
+      .filter(Boolean)
+      .slice(0, 4);
+
+    return res.json(orderedMovies);
+
+  } catch (error) {
+    return res.status(500).json({ error: "Recommendation failed" });
+  }
+};
