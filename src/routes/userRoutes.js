290da5256ba5f6748750b5c63cd1200189f86ce0
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-04-12 14:58:47 +0545

    feat(userRoutes): route the getallUsers , delteusers and updateusers

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,8 +6,12 @@ import {
   loginUser,
   getUserProfile,
   logoutUser,
+  getAllUsers,
+  deleteUser,
+  updateUserRole,
 } from "../controllers/user.controller.js";
 import verifyJWT from "../middleware/verifyJwt.middleware.js";
+import roleVerify from "../middleware/role.middleware.js";
 
 const userRouter = Router();
 
@@ -17,5 +21,8 @@ userRouter.post("/login", loginUser);
 userRouter.post("/logout", verifyJWT, logoutUser);
 userRouter.put("/updatePlan", verifyJWT, updateUserPlan);
 userRouter.get("/profile", verifyJWT, getUserProfile);
+userRouter.get("/all", verifyJWT, roleVerify, getAllUsers);
+userRouter.delete("/:userId", verifyJWT, roleVerify, deleteUser);
+userRouter.put("/:userId/role", verifyJWT, roleVerify, updateUserRole);
 
 export { userRouter };
